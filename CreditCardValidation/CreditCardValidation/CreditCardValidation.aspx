19e1285b8f451440472b47cc738945378d024fe2
caglarosman <cglrr.osman@gmail.com>
2022-04-04 10:42:16 +0300

    Credit Card Validation System in Web - ASP.NET
    In this section we developed the front end of a credit card payment system and added the algorithm that allows it to be verified.

---
+++ CreditCardValidation/CreditCardValidation/CreditCardValidation.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreditCardValidation.aspx.cs" Inherits="CreditCardValidation.CreditCardValidation" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="float:left;"><asp:Panel ID="PanelCreditCardValidation" runat="server" BackColor="WhiteSmoke" Height="330px" Width="250px" Font-Bold="False" BorderStyle="Solid" BorderWidth="2px">
+            <div style="margin: 20px 0 0 0; padding-left: 20px; padding-top: 10px">
+                <asp:Label ID="LabelPaymentDetails" runat="server" Text="Payment details" ForeColor="#C2BABA"></asp:Label>
+            </div>
+            <div style="margin: 0 0 0 20px">
+                <img alt="" src="img/visa.png" style="height: 40px;" />
+                <img alt="" src="img/visa-electron.png" style="height: 40px;" />
+                <img alt="" src="img/mastercard.png" style="height: 22px; padding-bottom: 9px" />
+                <img alt="" src="img/maestro.png" style="height: 40px;" />
+                <img alt="" src="img/discover.png" style="height: 27px; padding-bottom: 5px" />
+            </div>
+            <div style="margin: 0 0 5px 0; padding-left: 20px; padding-top: 5px">
+                <asp:Label ID="LabelCardNumber" runat="server" Text="Card number" ForeColor="#C2BABA"></asp:Label>
+                <div style="margin: 0 0 5px 0; padding-top: 5px">
+                    <div style="float:left;"><asp:TextBox ID="TextBoxCardNumber" runat="server" Width="170px" MaxLength="19" OnTextChanged="TextBoxCardNumber_TextChanged" AutoPostBack="True" BorderStyle="Solid"></asp:TextBox></div>
+                   <div style="margin: 1px 0 0 0; float:left;"><img ID="ImageBoxCardType" runat="server" alt="" src="" style="height: 25px; width: 30px; margin: 0px 0 0 10px; border-style:none;" visible="false"/></div>
+                </div>
+            </div>
+            <div style="margin: 0 0 5px 0; padding-left: 20px; padding-top: 5px; float:left">
+                <asp:Label ID="LabelExpiryDate" runat="server" Text="Expiry date " ForeColor="#C2BABA"></asp:Label>
+                <asp:Label ID="LabelMMYY" runat="server" Text="MM/YY" ForeColor="#C2BABA" Font-Size="XX-Small"></asp:Label>
+                <div style="margin: 0 0 5px 0; padding-top: 5px">
+                    <asp:TextBox ID="TextBoxExpiryDate" runat="server" Width="135px" MaxLength="50" ToolTip="MM/YY" TextMode="Month" BorderStyle="Solid" OnTextChanged="TextBoxExpiryDate_TextChanged"  AutoPostBack="True"></asp:TextBox>
+                </div>
+            </div>
+            <div style="margin: 0 0 5px 5px; padding-left: 20px; padding-top: 5px; float:left">
+                <div style="text-align:right"><asp:Label ID="LabelCVV" runat="server" Text="CVV" ForeColor="#C2BABA"></asp:Label></div>
+                <div style="margin: 0 0 5px -20px; padding-top: 5px; text-align:right">
+                    <asp:TextBox ID="TextBoxCVV" runat="server" Width="55px" TextMode="SingleLine" MaxLength="3" BorderStyle="Solid" AutoPostBack="True" OnTextChanged="TextBoxCVV_TextChanged"></asp:TextBox>
+                </div>
+            </div>
+
+            <div style="margin: 0px 0 5px 0; padding-left: 20px; padding-top: 5px; float:left">
+                <asp:Label ID="LabelNameOnCard" runat="server" Text="Name on card" ForeColor="#C2BABA"></asp:Label>
+                <div style="margin: 0 0 5px 0; padding-top: 5px">
+                    <asp:TextBox ID="TextBoxNameOnCard" runat="server" Width="200px" BorderStyle="Solid" AutoPostBack="True" OnTextChanged="TextBoxNameOnCard_TextChanged"></asp:TextBox>
+                    <div style="margin:20px 0 0 40px"><asp:Button ID="ButtonSubmit" runat="server" Height="25px" Text="Submit" Width="130px" OnClick="ButtonSubmit_Click" /></div>
+                </div>
+            </div>
+
+
+        </asp:Panel></div>
+        <div ID="PaymentResult" runat="server" style="width:550px; height:250px; float:left; margin:70px 0 0 50px; visibility:hidden">
+            <img src="img/checked.png" width="100px" style="float:left"/>
+            <span style="float: left; padding: 50px 0 50px 50px; font:bold 20px arial; width: 400px;">Ödeme işlemi başarıyla gerçekleştirildi!</span>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CreditCardValidation/CreditCardValidation/CreditCardValidation.aspx.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CreditCardValidation
+{
+    public partial class CreditCardValidation : System.Web.UI.Page
+    {
+        enum CardType { NONE, VISA, VISA_ELECTRON, MASTERCARD, MAESTRO, DISCOVER };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            if (IsPaymentOK())
+                PaymentResult.Style.Add(HtmlTextWriterStyle.Visibility, "visible");
+        }
+
+        private bool CheckValidity()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsPaymentOK()
+        {
+            if (!IsCreditCardNumberOKForLuhn())
+                return false;
+            else if (FindCardType(TextBoxCardNumber.Text) == CardType.NONE)
+                return false;
+            else
+                return true;
+        }
+
+        private bool IsCreditCardNumberOKForLuhn()
+        {
+            int sum = 0;
+            bool doubleDigit = false;
+
+            for (int i = TextBoxCardNumber.Text.Length - 1; i >= 0; i--)
+            {
+                if (doubleDigit)
+                {
+                    if ((int.Parse(TextBoxCardNumber.Text[i].ToString()) * 2) >= 10)
+                    {
+                        sum += (int.Parse(TextBoxCardNumber.Text[i].ToString()) * 2) % 10 + 1;
+                    }
+                    else
+                    {
+                        sum += int.Parse(TextBoxCardNumber.Text[i].ToString()) * 2;
+                    }
+                }
+                else
+                {
+                    sum += int.Parse(TextBoxCardNumber.Text[i].ToString());
+                }
+                doubleDigit = !doubleDigit;
+
+            }
+
+            if (sum % 10 == 0)
+                return true;
+            else
+                return false;
+
+
+
+        }
+
+        private CardType FindCardType(string cardNumber)
+        {
+            try
+            {
+                if (cardNumber.Substring(0, 4) == "4026" || cardNumber.Substring(0, 6) == "417500" ||
+                         cardNumber.Substring(0, 4) == "4405" || cardNumber.Substring(0, 4) == "4508" ||
+                        cardNumber.Substring(0, 4) == "4844" || cardNumber.Substring(0, 4) == "4913" ||
+                        cardNumber.Substring(0, 4) == "4917")
+                {
+                    return CardType.VISA_ELECTRON;
+                }
+                else if (cardNumber.Substring(0, 1) == "4")
+                {
+                    return CardType.VISA;
+                }
+                else if ((((int.Parse(cardNumber.Substring(0, 2))) >= 51 && (int.Parse(cardNumber.Substring(0, 2))) <= 55)) ||
+                    (((int.Parse(cardNumber.Substring(0, 6))) >= 222100 && (int.Parse(cardNumber.Substring(0, 6))) <= 272099)))
+                {
+                    return CardType.MASTERCARD;
+                }
+                else if ((((int.Parse(cardNumber.Substring(0, 6))) >= 500000 && (int.Parse(cardNumber.Substring(0, 6))) <= 509999)) ||
+                    (((int.Parse(cardNumber.Substring(0, 6))) >= 560000 && (int.Parse(cardNumber.Substring(0, 6))) <= 589999)) ||
+                    (((int.Parse(cardNumber.Substring(0, 6))) >= 600000 && (int.Parse(cardNumber.Substring(0, 6))) <= 699999)))
+                {
+                    return CardType.MAESTRO;
+                }
+                else if (cardNumber.Substring(0, 4) == "6011" ||
+                    (((int.Parse(cardNumber.Substring(0, 6))) >= 622126 && (int.Parse(cardNumber.Substring(0, 6))) <= 622925)) ||
+                    (((int.Parse(cardNumber.Substring(0, 3))) >= 644 && (int.Parse(cardNumber.Substring(0, 3))) <= 649)) ||
+                    cardNumber.Substring(0, 2) == "65")
+                {
+                    return CardType.DISCOVER;
+                }
+                else
+                {
+                    return CardType.NONE;
+                }
+            }
+            catch (Exception)
+            {
+                TextBoxCardNumber.Style.Add(HtmlTextWriterStyle.BorderColor, "#FF0000");
+                TextBoxCardNumber.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+                return CardType.NONE;
+            }
+            
+        }
+
+        protected void TextBoxCardNumber_TextChanged(object sender, EventArgs e)
+        {
+            CardType cardType = FindCardType(TextBoxCardNumber.Text);
+            bool isOK = IsCreditCardNumberOKForLuhn();
+
+            if (!isOK || cardType == CardType.NONE || !TextBoxCardNumber.Text.All(char.IsDigit))
+            {
+                TextBoxCardNumber.Style.Add(HtmlTextWriterStyle.BorderColor, "#FF0000");
+                TextBoxCardNumber.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+                ImageBoxCardType.Visible = false;
+                return;
+            }
+            else
+            {
+                TextBoxCardNumber.Style.Add(HtmlTextWriterStyle.BorderColor, "#00FF00");
+                TextBoxCardNumber.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+            }
+
+            //if(TextBoxExpiryDate.)
+
+            switch (cardType)
+            {
+                case CardType.NONE:
+                    ImageBoxCardType.Visible = true;
+                    ImageBoxCardType.Src = "https://localhost:44312/img/visa.png";
+                    break;
+                case CardType.VISA:
+                    ImageBoxCardType.Visible = true;
+                    ImageBoxCardType.Src = "https://localhost:44312/img/visa.png";
+                    break;
+                case CardType.VISA_ELECTRON:
+                    ImageBoxCardType.Visible = true;
+                    ImageBoxCardType.Src = "https://localhost:44312/img/visa-electron.png";
+                    break;
+                case CardType.MASTERCARD:
+                    ImageBoxCardType.Visible = true;
+                    ImageBoxCardType.Src = "https://localhost:44312/img/mastercard.png";
+                    break;
+                case CardType.MAESTRO:
+                    ImageBoxCardType.Visible = true;
+                    ImageBoxCardType.Src = "https://localhost:44312/img/maestro.png";
+                    break;
+                case CardType.DISCOVER:
+                    ImageBoxCardType.Visible = true;
+                    ImageBoxCardType.Src = "https://localhost:44312/img/discover.png";
+                    break;
+                default:
+                    ImageBoxCardType.Visible = false;
+                    break;
+            }
+        }
+
+
+        protected void TextBoxExpiryDate_TextChanged(object sender, EventArgs e)
+        {
+            DateTime expiryDate = DateTime.Parse(TextBoxExpiryDate.Text);
+            if (expiryDate < DateTime.Now)
+            {
+                TextBoxExpiryDate.Style.Add(HtmlTextWriterStyle.BorderColor, "#FF0000");
+                TextBoxExpiryDate.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+            }
+            else
+            {
+                TextBoxExpiryDate.Style.Add(HtmlTextWriterStyle.BorderColor, "#00FF00");
+                TextBoxExpiryDate.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+            }
+        }
+
+        protected void TextBoxCVV_TextChanged(object sender, EventArgs e)
+        {
+            if (TextBoxCVV.Text == String.Empty || TextBoxCVV.Text.Length < 3 || TextBoxCVV.Text.Length > 3)
+            {
+                TextBoxCVV.Style.Add(HtmlTextWriterStyle.BorderColor, "#FF0000");
+                TextBoxCVV.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+            }
+            else
+            {
+                TextBoxCVV.Style.Add(HtmlTextWriterStyle.BorderColor, "#00FF00");
+                TextBoxCVV.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+            }
+        }
+
+        protected void TextBoxNameOnCard_TextChanged(object sender, EventArgs e)
+        {
+            if (TextBoxNameOnCard.Text.Any(char.IsDigit))
+            {
+                TextBoxNameOnCard.Style.Add(HtmlTextWriterStyle.BorderColor, "#FF0000");
+                TextBoxNameOnCard.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+            }
+            else
+            {
+                TextBoxNameOnCard.Style.Add(HtmlTextWriterStyle.BorderColor, "#00FF00");
+                TextBoxNameOnCard.Style.Add(HtmlTextWriterStyle.BorderWidth, "1px");
+            }
+        }
+    }
+}
